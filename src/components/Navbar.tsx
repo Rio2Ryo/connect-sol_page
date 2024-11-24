8c5625354a815e3ae8cfaf334eb7405527e7e62f
Rio2Ryo <common.gifted.tokyo@gmail.com>
2024-11-24 19:21:52 +0900

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
             <img
               src="/conect-sol_logo.png"
               alt="コネクトソル"
-              className="h-28"
+              className="h-32"
             />
           </Link>
 
